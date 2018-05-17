39da4fc7139565090b05b9f66251a5fbdde1a59f
ahanneman <alexander.hanneman@optelgroup.com>
2018-05-17 09:24:19 -0500

    Replica Set with Load Balancer demo

---
+++ ConsoleSpammer/ConsoleSpammer/Program.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ConsoleSpammer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Run();
+            Console.ReadLine();
+        }
+
+        static void Run()
+        {
+            Parallel.Invoke(GetMachineName, 
+                GetMachineName, 
+                GetMachineName, 
+                GetMachineName, 
+                GetMachineName, 
+                GetMachineName, 
+                GetMachineName, 
+                GetMachineName, 
+                GetMachineName, 
+                GetMachineName,
+                GetMachineName,
+                GetMachineName);
+        }
+
+        static async void GetMachineName()
+        {
+            var httpClient = new HttpClient();
+            var response = await httpClient.GetAsync("http://104.43.138.208/api/MachineName");
+            var machineName = await response.Content.ReadAsStringAsync();
+            Console.WriteLine(machineName);
+        }
+    }
+}
---
+++ Kubernetes.TestApp/Controllers/TestController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Kubernetes.TestApp.Controllers
+{
+    [Route("api/")]
+    public class ValuesController : Controller
+    {
+        [HttpGet("")]
+        public string Get(int id)
+        {
+            return "System is up and running";
+        }
+
+        [HttpGet("MachineName")]
+        public string GetMachineName(int id)
+        {
+            return System.Environment.MachineName;
+        }
+    }
+}
